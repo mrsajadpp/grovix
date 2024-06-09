c10627ce53e1b463ea87d974e693d37170719bf9
mrsajadpp <mrsajadpp@gmail.com>
2024-06-09 22:28:23 +0530

    structured data added for tools

--- routes/tools.js
+++ routes/tools.js
@@ -9,17 +9,17 @@ var router = express.Router();
 
 // CSS Minifier
 router.get('/developer/tools/minify/css', (req, res, next) => {
-    res.render('developer/tools/minify/css/index', { title: "CSS Minifier and Compressor", style: ["tools"], description: "Enhance your CSS with ease using our CSS Minifier tool. Simply input your CSS code, press 'Minify', and receive a sleek, optimized version immediately. Ideal for web developers and designers looking to enhance website performance.", url: 'https://www.grovixlab.com/developer/tools/minify/css', user: req.session && req.session.user ? req.session.user : false });
+    res.render('developer/tools/minify/css/index', { title: "CSS Minifier and Compressor", tool: true, style: ["tools"], description: "Enhance your CSS with ease using our CSS Minifier tool. Simply input your CSS code, press 'Minify', and receive a sleek, optimized version immediately. Ideal for web developers and designers looking to enhance website performance.", url: 'https://www.grovixlab.com/developer/tools/minify/css', user: req.session && req.session.user ? req.session.user : false });
 });
 
 // JS Minifier
 router.get('/developer/tools/minify/js', (req, res, next) => {
-    res.render('developer/tools/minify/js/index', { title: "JS Minifier and Compressor", style: ["tools"], description: "Enhance your JavaScript with ease using our JS Minifier tool. Simply input your JS code, press 'Minify', and receive a sleek, optimized version immediately. Ideal for web developers and designers looking to enhance website performance.", url: 'https://www.grovixlab.com/developer/tools/minify/js', user: req.session && req.session.user ? req.session.user : false });
+    res.render('developer/tools/minify/js/index', { title: "JS Minifier and Compressor", tool: true, style: ["tools"], description: "Enhance your JavaScript with ease using our JS Minifier tool. Simply input your JS code, press 'Minify', and receive a sleek, optimized version immediately. Ideal for web developers and designers looking to enhance website performance.", url: 'https://www.grovixlab.com/developer/tools/minify/js', user: req.session && req.session.user ? req.session.user : false });
 });
 
 // HTML Minifier
 router.get('/developer/tools/minify/html', (req, res, next) => {
-    res.render('developer/tools/minify/html/index', { title: "HTML Minifier and Compressor", style: ["tools"], description: "Enhance your HTML with ease using our HTML Minifier tool. Simply input your HTML code, press 'Minify', and receive a sleek, optimized version immediately. Ideal for web developers and designers looking to enhance website performance.", url: 'https://www.grovixlab.com/developer/tools/minify/js', user: req.session && req.session.user ? req.session.user : false });
+    res.render('developer/tools/minify/html/index', { title: "HTML Minifier and Compressor", tool: true, style: ["tools"], description: "Enhance your HTML with ease using our HTML Minifier tool. Simply input your HTML code, press 'Minify', and receive a sleek, optimized version immediately. Ideal for web developers and designers looking to enhance website performance.", url: 'https://www.grovixlab.com/developer/tools/minify/js', user: req.session && req.session.user ? req.session.user : false });
 });
 
 // Tools Sitemap.xml
