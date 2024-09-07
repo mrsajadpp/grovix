9ddf719ade5239e60d393d34c4f8b57397e8528c
mrsajadpp <mrsajadpp@gmail.com>
2024-09-07 12:15:38 +0000

    bug fixed

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
  
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.DB_STRING}`);
+    await mongoose.connect(`${process.env.DB_STRING}/grovixlab`);
     console.log('MongoDB connected');
   } catch (error) {
 console.error(error);
